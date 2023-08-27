10d7b909afc940f338fe18fb2b1139a1d48b07cd
SujethKannan <sujethkannan98@gmail.com>
2023-08-27 20:23:55 +0530

    commit-5

--- files_io/sync.js
+++ files_io/sync.js
@@ -4,6 +4,6 @@ const filepath=path.resolve(__dirname,'hello.txt');
 // console.log(pathh.resolve(__dirname,'hello.txt'));
 const content= fs.readFileSync(filepath,'utf-8');            // Read the contents of the file synchronously
 const modifiedcontent=content.toUpperCase();                // Convert the contents to uppercase
- fs.writeFileSync(filepath,modifiedcontent,'utf-8');        // Write the updated contents back to the file synchronously
+ fs.writeFileSync(filepath,modifiedcontent);        // Write the updated contents back to the file synchronously
 
 console.log(modifiedcontent);
