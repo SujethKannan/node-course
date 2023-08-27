50cc7fc18542164ae5fa1e97740d9224bc05c49b
SujethKannan <sujethkannan98@gmail.com>
2023-08-27 12:39:42 +0530

    commit-5

--- web_protocal/basic.js
+++ web_protocal/basic.js
@@ -1,14 +1,14 @@
 const http=require('http');
 const HOSTNAME='Localhost';
-const PORT='20202';
+const PORT='2020';
 const data ={
     name:'Chris Evans',
     age:'39',
     designation:'senior developer'
 }
 const server=http.createServer((request,response)=>{
-     //using 'application/jason' for an object, use 'plain/text' for a string
-    response.writeHead(400,{"Content-Type":'application/jason'});      
+     //using 'application/json' for an object, use 'plain/text' for a string
+    response.writeHead(400,{"Content-Type":'application/json'});      
     response.end(JSON.stringify(data));
 
 })
